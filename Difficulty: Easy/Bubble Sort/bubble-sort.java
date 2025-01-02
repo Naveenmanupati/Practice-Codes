14fa77e738fb4277f979719ca71bf0c191ff3b54
Naveenmanupati <66304392+Naveenmanupati@users.noreply.github.com>
2025-01-02 14:16:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.java
+++ Difficulty: Easy/Bubble Sort/bubble-sort.java
@@ -9,21 +9,25 @@ import java.util.*;
 // User function Template for Java
 
 class Solution {
-    
-
+    // Function to sort the array using bubble sort algorithm.
     public static void bubbleSort(int arr[]) {
-        int n=arr.length;
+        int n = arr.length;
         for(int i=0;i<n-1;i++){
+            boolean swapped =false;
             for(int j=0;j<n-i-1;j++){
-                if (arr[j]>arr[j+1]){
-                    int temp = arr[j];
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped=true;
                 }
             }
-        }
-        //return bubbleSort;
+            if(!swapped){
+                break;
+            }
     }
+    
+}
 }
 
 //{ Driver Code Starts.
